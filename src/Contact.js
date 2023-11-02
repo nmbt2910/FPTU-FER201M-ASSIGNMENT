11bf2122cab89040f420c8a25780864fd13979df
fantacola0x0 <96630960+fantacola0x0@users.noreply.github.com>
2023-11-02 10:40:39 +0700

    contact working

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   Typography,
@@ -7,9 +7,54 @@ import {
   Container,
   Paper,
   Grid,
+  Snackbar,
 } from '@mui/material';
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('https://6540cf3145bedb25bfc2a8ea.mockapi.io/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email, message }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to submit form');
+      }
+
+      setName('');
+      setEmail('');
+      setMessage('');
+      setIsSnackbarOpen(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSnackbarClose = () => {
+    setIsSnackbarOpen(false);
+  };
+
   return (
     <Box
       sx={{
@@ -22,15 +67,25 @@ function Contact() {
     >
       <Container maxWidth="sm">
         <Paper elevation={3} sx={{ padding: '20px', textAlign: 'center' }}>
-          <Typography variant="h4" component="h1" sx={{ marginBottom: '20px'}}>
-            Contact AnimeHub
+          <Typography variant="h4" component="h1" sx={{ marginBottom: '20px' }}>
+            Contact BlogPage
           </Typography>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <TextField label="Name" fullWidth />
+              <TextField
+                label="Name"
+                fullWidth
+                value={name}
+                onChange={handleNameChange}
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField label="Email" fullWidth />
+              <TextField
+                label="Email"
+                fullWidth
+                value={email}
+                onChange={handleEmailChange}
+              />
             </Grid>
             <Grid item xs={12}>
               <TextField
@@ -38,15 +93,23 @@ function Contact() {
                 multiline
                 rows={4}
                 fullWidth
+                value={message}
+                onChange={handleMessageChange}
               />
             </Grid>
             <Grid item xs={12}>
-              <Button variant="contained" color="primary" fullWidth>
+              <Button variant="contained" color="primary" fullWidth onClick={handleSubmit}>
                 Submit
               </Button>
             </Grid>
           </Grid>
         </Paper>
+        <Snackbar
+          open={isSnackbarOpen}
+          autoHideDuration={3000}
+          onClose={handleSnackbarClose}
+          message="Message sent successfully!"
+        />
       </Container>
     </Box>
   );
